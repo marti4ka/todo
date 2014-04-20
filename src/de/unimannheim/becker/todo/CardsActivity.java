d13cb4a9b4272d131cedcb4706cb39fe77f0c8d5
shiba <shiba@shiba-TOSH>
2014-04-20 10:58:12 +0200

    deleted unused activity

--- src/de/unimannheim/becker/todo/CardsActivity.java
+++ src/de/unimannheim/becker/todo/CardsActivity.java
@@ -231,34 +231,4 @@ public class CardsActivity extends Activity {
 		mTitle = title;
 		getActionBar().setTitle(mTitle);
 	}
-
-	/**
-	 * Fragment that appears in the "content_frame", change the view
-	 */
-	public static class TheNewFragment extends Fragment {
-		public static final String ARG_MENU_ITEM_NUMBER = "planet_number";
-
-		public TheNewFragment() {
-			// Empty constructor required for fragment subclasses
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			// @todo change the view
-			View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
-			// here we get the id of the clicked item from the list
-			// switch/case
-			int i = getArguments().getInt(ARG_MENU_ITEM_NUMBER);
-			// get the title from the strings
-			String planet = getResources().getStringArray(R.array.menu_array)[i];
-
-			// load the image and the layout
-			int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()), "drawable",
-					getActivity().getPackageName());
-			((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
-
-			getActivity().setTitle(planet);
-			return rootView;
-		}
-	}
 }
---
+++ src/de/unimannheim/becker/todo/TheNewFragment.java
@@ -0,0 +1,40 @@
+package de.unimannheim.becker.todo;
+
+import java.util.Locale;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Fragment that appears in the "content_frame", change the view
+ */
+public class TheNewFragment extends Fragment {
+	public static final String ARG_MENU_ITEM_NUMBER = "planet_number";
+
+	public TheNewFragment() {
+		// Empty constructor required for fragment subclasses
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// @todo change the view
+		View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
+		// here we get the id of the clicked item from the list
+		// switch/case
+		int i = getArguments().getInt(ARG_MENU_ITEM_NUMBER);
+		// get the title from the strings
+		String planet = getResources().getStringArray(R.array.menu_array)[i];
+
+		// load the image and the layout
+		int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()), "drawable",
+				getActivity().getPackageName());
+		((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+
+		getActivity().setTitle(planet);
+		return rootView;
+	}
+}
