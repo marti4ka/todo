0be59b0f4eb511d485c1d84ab8a29da7628ac0bf
Martina Dukadinova <martina.dukadinova@sap.com>
2014-05-02 14:46:55 +0200

    test fixed

--- FirstActivityTest/src/de/unimannheim/becker/todo/test/ItemDAOTest.java
+++ FirstActivityTest/src/de/unimannheim/becker/todo/test/ItemDAOTest.java
@@ -42,7 +42,7 @@ public class ItemDAOTest extends ActivityInstrumentationTestCase2<CardsActivity>
         assertTrue(success);
         Item[] items = sut.getItems();
         assertEquals(1, items.length);
-        boolean res = sut.archiveItem(item.getId());
+        boolean res = sut.archiveItem(items[0].getId());
         assertTrue(res);
         items = sut.getItems();
         Item[] archived = sut.getArchived();
